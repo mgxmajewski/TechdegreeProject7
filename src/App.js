4eb36d628a15de7cca7d3fd5d2368c54c9b594b4
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-23 00:07:52 +0100

    Logo JSON from flickr with 24 pictures Initially with cats

--- src/App.js
+++ src/App.js
@@ -24,12 +24,13 @@ class App extends Component {
     }
 
     performSearch = (query = 'cats') => {
-        axios.get(`https://api.giphy.com/v1/gifs/search?api_key=8kPPAvSPwZGTj0QyLzVO7ovMegXe79v5&q=${query}&limit=24&offset=0&rating=g&lang=en`)
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
             .then(response => {
                 this.setState({
-                    gifs: response.data.data,
-                    loading: false
+                    pictures: response.data.photos.photo,
+                    loading: false,
                 })
+                console.log(this.state.pictures)
             })
             .catch(error => {
                 console.log('Error fetching and parsing data', error)
@@ -43,11 +44,11 @@ class App extends Component {
                 <SearchBar />
                 <MainNav />
                 <div className="photo-container">
-                    {
-                        (this.state.loading)
-                            ? <p>Loading...</p>
-                            : <GifList data={this.state.gifs} />
-                    }
+                    {/*{*/}
+                    {/*    (this.state.loading)*/}
+                    {/*        ? <p>Loading...</p>*/}
+                    {/*        : <GifList data={this.state.gifs} />*/}
+                    {/*}*/}
                 </div>
 
             </Router>

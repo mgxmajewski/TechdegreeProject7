a3ddf7cbee59d1636d00cb876da9984e7a3d0f7f
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-23 20:34:39 +0100

    Refactor to use App for Routing and Home for content

--- src/App.js
+++ src/App.js
@@ -1,53 +1,15 @@
 import React, { Component } from 'react';
-import SearchBar from "./components/SearchBar";
-import MainNav from "./components/MainNav";
-import axios from "axios";
+import Home from "./components/Home";
 
 import {
-    BrowserRouter as Router
+    BrowserRouter as Router,
+    Route
 } from "react-router-dom";
 
-import apiKey from "./config";
-import Gallery from "./components/Gallery";
-
-class App extends Component {
-
-    constructor() {
-        super()
-        this.state = {
-            pictures: [],
-            loading: true
-        }
-    }
-
-    componentDidMount() {
-        this.performSearch()
-    }
-
-    performSearch = (query = 'cats') => {
-        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-            .then(response => {
-                this.setState({
-                    pictures: response.data.photos.photo,
-                    loading: false,
-                })
-                console.log(this.state.pictures)
-            })
-            .catch(error => {
-                console.log('Error fetching and parsing data', error)
-            })
-    }
-
-
-    render() {
-        return (
-            <Router>
-                <SearchBar search={this.performSearch}/>
-                <MainNav />
-                <Gallery data={this.state.pictures} />
-            </Router>
-        );
-    }
-}
+const App = () => (
+    <Router>
+        <Route exact path="/" component={Home} />
+    </Router>
+)
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import SearchBar from "./SearchBar";
+import MainNav from "./MainNav";
+import axios from "axios";
+
+
+import {
+    BrowserRouter as Router
+} from "react-router-dom";
+
+import apiKey from "../config";
+import Gallery from "./Gallery";
+
+export default class Home extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            pictures: [],
+            loading: true
+        }
+    }
+
+    componentDidMount() {
+        this.performSearch()
+    }
+
+    performSearch = (query = 'cats') => {
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+            .then(response => {
+                this.setState({
+                    pictures: response.data.photos.photo,
+                    loading: false,
+                })
+                console.log(this.state.pictures)
+            })
+            .catch(error => {
+                console.log('Error fetching and parsing data', error)
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar search={this.performSearch}/>
+                <MainNav />
+                <Gallery data={this.state.pictures} />
+            </div>
+        );
+    }
+}

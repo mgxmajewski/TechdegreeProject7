dcf898b800e0083afbf0f5f5c67af1ba71c4532b
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-27 00:19:31 +0100

    Refactor Home to use hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from "./SearchBar";
 import MainNav from "./MainNav";
 import axios from "axios";
@@ -12,49 +12,27 @@ import {
 import apiKey from "../config";
 import Gallery from "./Gallery";
 
-export default class Home extends Component {
+export default function Home() {
 
-    constructor() {
-        super()
-        this.state = {
-            pictures: [],
-            loading: true,
-            query: ''
-        }
-    }
+    const [data, setData] = useState([])
+    const [query, setQuery] = useState('cats')
+    const [isLoading, setIsLoading] = useState(true)
 
-    componentDidMount() {
-        let query = this.props.location.pathname
-        if (query === '/') {
-            this.performSearch('cats')
-        } else {
-            this.performSearch(query)
-        }
-        console.log('query:' + query)
-    }
-
-    performSearch = (query) => {
+    useEffect( () => {
         axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-            .then(response => {
-                this.setState({
-                    pictures: response.data.photos.photo,
-                    loading: false,
-                    query: this.props.location.pathname
-                })
-                // console.log(this.state.pictures)
-            })
-            .catch(error => {
-                console.log('Error fetching and parsing data', error)
-            })
-    }
+            .then(response => setData(response.data.photos.photo))
+            .catch(error => console.log('Error fetching and parsing data', error))
+            .finally(()=> setIsLoading(false))
+    }, [query])
 
-    render() {
+    const performSearch = (value) => setQuery(value)
+
         return (
             <div className="container">
-                <SearchBar search={this.performSearch} />
-                <MainNav search={this.performSearch} />
-                <Gallery data={this.state} />
+                <SearchBar search={performSearch} />
+                <MainNav search={performSearch} />
+                <Gallery data={data} isLoading={isLoading} />
             </div>
         );
-    }
+
 }

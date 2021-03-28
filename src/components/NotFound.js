961c9a24cf133fe3ad25733be8ea6dcf357b4c19
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-28 19:04:16 +0200

    Add NotFound component

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from "./Home";
+
+const NotFound = () => (
+    <div className='photo-container not-found'>
+        <h2>Page Not Found</h2>
+    </div>
+);
+
+export default NotFound;
